e906f03e221451eabd51241e49620a9c46fd5bec
Shivam <Shivamptiwari55812@gmail.com>
2026-05-08 00:25:23 +0530

    Used cars

--- Pythoncore/oops.py
+++ Pythoncore/oops.py
@@ -1,6 +1,8 @@
-from copyreg import constructor
+nums = [7,9,3,5,0]
 
-
-print("Shivam Tiwari")
-class Shivam:
-    constructor
+def solution(nums):
+    if not nums:
+        return None
+    nums_sorted = sorted(nums)
+    return nums_sorted[-2]
+print(solution(nums))
