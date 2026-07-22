a583fe28165e90bba0579bec4cfe9c15f414893a
Shivam <Shivamptiwari55812@gmail.com>
2026-07-22 09:57:27 +0530

    Revising stuff

--- Pythoncore/sorting.py
+++ Pythoncore/sorting.py
@@ -1,4 +1,3 @@
-from concurrent.futures import InvalidStateError
 
 
 arr = [ 34 ,42 ,1 , 17 ,98]
@@ -12,7 +11,7 @@ def bubble_sort(arr:list):  #Place the biggest at the last
     return arr
 print(bubble_sort(arr))
 
-def selection_sort(arr:list): # Place the smallest at the sorted arr
+def selection_sort(arr:list): # Finds smallest element throughout the iteration then swaps 
    if not arr:
        return None
    for i in range(len(arr)-1):
@@ -50,3 +49,44 @@ def bubble_sort_1(arr):
     print(arr)
     return arr
 bubble_sort_1(arr)
+
+
+arr34= [ 45,67,34,28,99]
+
+def bubbly_sort(arr):
+    if arr is None:
+        return []
+    for i in range(0,len(arr)):
+        for j in range(0,len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+print("Bubble_Sort",bubbly_sort(arr34))
+
+arr34= [ 45,67,34,28,99]
+
+def selection_sort_rev(arr):
+    if not arr:
+        return []
+    for i in range(0,len(arr)):
+        min_idx = i
+        for j in range(i+1,len(arr)-1):
+            if arr[min_idx]> arr[j]:
+                min_idx = j
+        arr[min_idx],arr[i] = arr[i],arr[min_idx]
+                
+    return arr
+print("Selection_sort_rev",selection_sort_rev(arr34))
+
+
+def insertion_sort_rev(arr):
+    if arr is None:
+        return None
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i-1
+        while arr[j] > key and j >= 0:
+                arr[j+1] = arr[j]
+                j-= 1
+        arr[j+1]=key
+    return arr
